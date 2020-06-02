1ddc0ea2c836f7f7990419100fff19843b44f71f
Dominik <leitner.dominik@hotmail.de>
2020-06-02 23:04:14 +0200

    add routes error-handling

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const usersRoute = require('./routes/users-routes');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use('/api/users', usersRoute);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || 'An unknown error occurred!' });
+});
 
 const { DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 const dbUrl = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0-d0m2c.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
