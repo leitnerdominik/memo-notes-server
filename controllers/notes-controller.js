33f5bf3ccdbf37e7e986bbe52c77c5d48e3c3fe7
Benkert, Marius <marius.benkert@student.fhws.de>
2020-06-08 17:07:00 +0200

    fixed small error

--- controllers/notes-controller.js
+++ controllers/notes-controller.js
@@ -4,7 +4,9 @@ const User = require('../models/user');
 const Note = require('../models/note');
 
 const createNote = async (req, res, next) => {
-  const { title, content, userId } = req.body;
+  const { title, content } = req.body;
+
+  const userId = req.userData.userId;
 
   let user;
   try {
@@ -44,8 +46,7 @@ const createNote = async (req, res, next) => {
     content: newNote.content,
     public: newNote.public,
     id: newNote._id,
-    
-  }
+  };
 
   res.status(201).json({ message: 'Note created!', note: newNote });
 };
@@ -135,19 +136,21 @@ const publishNote = async (req, res, next) => {
     );
   }
 
-  if(note.creator.toString() !== req.userData.userId) {
-    return next(new HttpError('You are not allowed to publish this note.', 403))
+  if (note.creator.toString() !== req.userData.userId) {
+    return next(
+      new HttpError('You are not allowed to publish this note.', 403)
+    );
   }
 
   note.public = true;
 
   try {
     await note.save();
-  } catch(err) {
-    return next(new HttpError('Publishing failed. Please try again.'))
+  } catch (err) {
+    return next(new HttpError('Publishing failed. Please try again.'));
   }
 
-  res.json({message: 'Note published!'})
+  res.json({ message: 'Note published!' });
 };
 
 const getNote = async (req, res, next) => {
@@ -156,20 +159,22 @@ const getNote = async (req, res, next) => {
   const { noteId, userId } = req.params;
   console.log(noteId, userId);
 
-
   let note;
   try {
     note = await Note.findById(noteId).populate('creator');
-  } catch(err) {
+  } catch (err) {
     return next(new HttpError('Getting note failed. Please try again.'));
   }
 
-  if(!note) {
-    return next(new HttpError('Could not fetch note with the provided id.'), 404);
+  if (!note) {
+    return next(
+      new HttpError('Could not fetch note with the provided id.'),
+      404
+    );
   }
 
-  if(note.creator._id.toString() !== userId && !note.public) {
-    return next(new HttpError('You are not allowed to fetch this note.', 403))
+  if (note.creator._id.toString() !== userId && !note.public) {
+    return next(new HttpError('You are not allowed to fetch this note.', 403));
   }
 
   const fetchedNote = {
@@ -177,26 +182,25 @@ const getNote = async (req, res, next) => {
     content: note.content,
     createdAt: note.createdAt,
     updatedAt: note.updatedAt,
-  }
+  };
 
-  res.json({message: 'Note fetched!', note: fetchedNote});
-}
+  res.json({ message: 'Note fetched!', note: fetchedNote });
+};
 
 const getNotes = async (req, res, next) => {
-
   let notes;
   try {
-    notes = await Note.find({creator: req.userData.userId}, '-creator -__v');
-  } catch(err) {
-    return next(new HttpError('Getting notes failed. Please try again.'))
+    notes = await Note.find({ creator: req.userData.userId }, '-creator -__v');
+  } catch (err) {
+    return next(new HttpError('Getting notes failed. Please try again.'));
   }
 
-  if(!notes) {
+  if (!notes) {
     return next(new HttpError('Could not find any note for this user.', 404));
   }
 
-  res.json({message: 'Notes fetched!', notes: notes})
-}
+  res.json({ message: 'Notes fetched!', notes: notes });
+};
 
 exports.createNote = createNote;
 exports.updateNote = updateNote;
